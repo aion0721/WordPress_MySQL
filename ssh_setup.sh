90fc092189e15823f93b2b5dc11be574a1fb0915
aion0721 <rudolf.aion@gmail.com>
2020-02-29 10:18:56 +0000

    Changed DB (from mysql to mariadb)

---
+++ ssh_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo Please input one argument.
+	exit 1
+fi
+
+target_name=$1
+
+echo ${target_name}
+
+ssh-copy-id pi@${target_name}
+
+scp /root/.ssh/id_rsa.pub pi@${target_name}
+
+ssh pi@${target_name} "sudo mkdir -m 700 /root/.ssh"
+
+ssh pi@${target_name} "sudo ssh-keygen -t rsa -m PEM -N '' -f /root/.ssh/id_rsa"
+
+ssh pi@${target_name} "sudo cp /tmp/id_rsa.pub /root/.ssh/authorized_keys"
