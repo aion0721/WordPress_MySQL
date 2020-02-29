439f7681e69bc61d27bfb4e264f61300a5e99d3d
aion0721 <rudolf.aion@gmail.com>
2020-02-29 12:27:25 +0000

    Copy SSH Key & Delete finished file.

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -11,10 +11,12 @@ echo ${target_name}
 
 ssh-copy-id pi@${target_name}
 
-scp /root/.ssh/id_rsa.pub pi@${target_name}
+scp /root/.ssh/id_rsa.pub pi@${target_name}:/tmp
 
 ssh pi@${target_name} "sudo mkdir -m 700 /root/.ssh"
 
 ssh pi@${target_name} "sudo ssh-keygen -t rsa -m PEM -N '' -f /root/.ssh/id_rsa"
 
 ssh pi@${target_name} "sudo cp /tmp/id_rsa.pub /root/.ssh/authorized_keys"
+
+rm ./pi@${target_name}
