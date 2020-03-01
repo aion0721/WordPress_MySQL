ccc88d5bc329530ce30f1196f5c745e57a51789b
aion0721 <rudolf.aion@gmail.com>
2020-03-01 01:41:01 +0000

    Delete sample determination

--- serverspec/spec/raspnetes02.local/sample_spec.rb
+++ serverspec/spec/raspnetes02.local/sample_spec.rb
@@ -1,40 +1,17 @@
 require 'spec_helper'
 
-describe package('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
-end
-
-describe package('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
-
-describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 # Checking SSH Port
 describe port(22) do
   it { should be_listening }
 end
 
 # Checking Installed Mysql
-describe package('mysql-server') do
+describe package('mariadb-server') do
   it { should be_installed }
 end
 
 # Checking Installed Mysql-Client
-describe package('mysql-client') do
+describe package('mariadb-client') do
   it { should be_installed }
 end
 
